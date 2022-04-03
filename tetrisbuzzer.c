26b98471022410907bf0296de85d2b699f74247d
FaliciaOng <ongfali@gmail.com>
2022-04-03 17:16:56 +0800

    tetris updated

--- tetrisbuzzer.c
+++ tetrisbuzzer.c
@@ -133,29 +133,40 @@
 // change this to whichever pin you want to use
 
 
-// notes of the moledy followed by the duration.
-// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
-// !!negative numbers are used to represent dotted notes,
-// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
-int melody[] = {
+int tetris_melody[] = {
 
-  // Never Gonna Give You Up - Rick Astley
-  // Score available at https://musescore.com/chlorondria_5/never-gonna-give-you-up_alto-sax
-  // Arranged by Chlorondria
-	 
-	 
-  NOTE_D5,2, NOTE_E5,8, NOTE_FS5,8, NOTE_D5,8, //13
-  NOTE_E5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,4, NOTE_A4,4,
-  REST,2, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8,
-  REST,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
-  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
-	
+  //Based on the arrangement at https://www.flutetunes.com/tunes.php?id=192
+  
+  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
+  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,8,  NOTE_A4,4,  NOTE_B4,8,  NOTE_C5,8,
 
-  NOTE_E5,-8, NOTE_E5,-8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,-8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //18
-  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,8, NOTE_A4,8, NOTE_A4,8, 
-  NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
-  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
-  NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_D5, -4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
+  NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, REST, 4,
+
+  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
+  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,8,  NOTE_A4,4,  NOTE_B4,8,  NOTE_C5,8,
+
+  NOTE_D5, -4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
+  NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, REST, 4,
+  
+
+  NOTE_E5,2,  NOTE_C5,2,
+  NOTE_D5,2,   NOTE_B4,2,
+  NOTE_C5,2,   NOTE_A4,2,
+  NOTE_GS4,2,  NOTE_B4,4,  REST,8, 
+  NOTE_E5,2,   NOTE_C5,2,
+  NOTE_D5,2,   NOTE_B4,2,
+  NOTE_C5,4,   NOTE_E5,4,  NOTE_A5,2,
+  NOTE_GS5,2,
+
 };
 
  
@@ -195,8 +206,8 @@ void initPWM()
 }
 
 // change this to make the song slower or faster
-int tempo = 180;
-int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+int tempo_tetris=144; 
+int notes = sizeof(tetris_melody) / sizeof(tetris_melody[0]) / 2;
 int divider = 0, noteDuration = 0;
 
 void setFreq(int freq)
@@ -225,22 +236,22 @@ void thread_buzzer (void *argument) {
 	
 	
 	for (;;) {
-		int wholenote = (60000 * 4) / tempo;
+		int wholenote = (60000 * 4) / tempo_tetris;
 		for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
 
 			// calculates the duration of each note
-			divider = melody[thisNote + 1];
-			if (divider > 0) {
-				// regular note, just proceed
-				noteDuration = (wholenote) / divider;
-			} else if (divider < 0) {
-				// dotted notes are represented with negative durations!!
-				noteDuration = (wholenote) / (divider*-1);
-				noteDuration *= 1.5; // increases the duration in half for dotted notes
-		}
+    divider = tetris_melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / (divider*-1);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
 
     // we only play the note for 90% of the duration, leaving 10% as a pause
-		setFreq( melody[thisNote]);
+		setFreq( tetris_melody[thisNote]);
 		osDelay(noteDuration * 0.9);
 
     // Wait for the specief duration before playing the next note.
@@ -254,9 +265,6 @@ void thread_buzzer (void *argument) {
 }
 
 
-	
-	
-
 int main (void) {
  
   // System Initialization
@@ -264,6 +272,7 @@ int main (void) {
 	initPWM();
 	//initGreenStrip();
 	osThreadNew(thread_buzzer, NULL, NULL);
+	
 	osKernelStart();                      		// Start thread execution
 	for (;;) {}
 		
