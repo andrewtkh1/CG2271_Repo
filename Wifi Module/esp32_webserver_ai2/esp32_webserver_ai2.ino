ebda26558b53eaf40f78e31415a8896e5b069d38
andrewtkh1 <andrewtkh1@gmail.com>
2022-04-04 19:36:39 +0800

    Updated ESP32 file.

--- Wifi Module/esp32_webserver_ai2/esp32_webserver_ai2.ino
+++ Wifi Module/esp32_webserver_ai2/esp32_webserver_ai2.ino
@@ -85,13 +85,15 @@ void loop() {
 // If disconnected, try to reconnect every 30 seconds.
   if ((WiFi.status() != WL_CONNECTED) && (millis() > wait30)) {
     Serial.println("Trying to reconnect WiFi...");
+    //Serial2.write(0x00);
     WiFi.disconnect();
     WiFi.begin(ssid, password);
     wait30 = millis() + 30000;
+    
   } 
   // Check if a client has connected..
   WiFiClient client = server.available();
-  if (!client) {
+  if (!client) {  
     return;
   }
    
